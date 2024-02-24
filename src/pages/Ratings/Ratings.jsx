04902242fe7c6ef31059194c6ef633dc2fd691e6
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-02-24 14:10:58 -0300

    adding button + responsiveness

--- src/pages/Ratings/Ratings.jsx
+++ src/pages/Ratings/Ratings.jsx
@@ -3,7 +3,7 @@ import './Ratings.css'
 export default function Ratings() {
     return (
         <div className="container" style={{ maxWidth: '700px' }}>
-            <div className="card border-0 shadow-lg rounded-4 p-5 mt-5 bg-light">
+            <div className="card border-0 shadow-lg rounded-4 py-5 px-3 p-md-5 mt-5 bg-light">
                 <div className="container">
                     <div className="row">
                         <div className="col-12 col-sm-7">
@@ -29,16 +29,67 @@ export default function Ratings() {
                         </div>
                     </div>
 
-                    <div className="card border-0 bg-transparent">
-                        <h5 className='fw-bold'>Reviews</h5>
+                    <div className="row p-0 p-sm-2 p-md-4">
+                        <div className="col-12">
+                            <h5>Reviews</h5>
 
-                        <div className="d-flex text-align-center justify-content-center gap-1">
-                            <h6>5</h6>
-                            <i className="bi bi-star-fill text-warning"></i>
+                            <div className="vstack gap-2">
+                                <div className="d-flex align-items-center gap-4">
+                                    <div className='d-flex gap-1'>
+                                        <span>5</span> <i className="bi bi-star-fill text-warning"></i>
+                                    </div>
+
+                                    <div className="progress h-5 w-100" >
+                                        <div className="progress-bar bg-warning" style={{ width: '100%' }}></div>
+                                    </div>
+                                </div>
+
+                                <div className="d-flex align-items-center gap-4">
+                                    <div className='d-flex gap-1'>
+                                        <span>4</span> <i className="bi bi-star-fill text-warning"></i>
+                                    </div>
+
+                                    <div className="progress h-5 w-100" >
+                                        <div className="progress-bar bg-warning" style={{ width: '80%' }}></div>
+                                    </div>
+                                </div>
+
+                                <div className="d-flex align-items-center gap-4">
+                                    <div className='d-flex gap-1'>
+                                        <span>3</span> <i className="bi bi-star-fill text-warning"></i>
+                                    </div>
+
+                                    <div className="progress h-5 w-100" >
+                                        <div className="progress-bar bg-warning" style={{ width: '60%' }}></div>
+                                    </div>
+                                </div>
+
+                                <div className="d-flex align-items-center gap-4">
+                                    <div className='d-flex gap-1'>
+                                        <span>2</span> <i className="bi bi-star-fill text-warning"></i>
+                                    </div>
+
+                                    <div className="progress h-5 w-100" >
+                                        <div className="progress-bar bg-warning" style={{ width: '40%' }}></div>
+                                    </div>
+                                </div>
+
+                                <div className="d-flex align-items-center gap-4">
+                                    <div className='d-flex gap-1'>
+                                        <span>1</span> <i className="bi bi-star-fill text-warning"></i>
+                                    </div>
+
+                                    <div className="progress h-5 w-100" >
+                                        <div className="progress-bar bg-warning" style={{ width: '20%' }}></div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                    </div>
 
-                        <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                            <div class="progress-bar" style={{ width: '25%' }}></div>
+                    <div className="row pt-4">
+                        <div className="col-12">
+                            <button className="btn btn-success btn-lg rounded-5">Write Review</button>
                         </div>
                     </div>
                 </div>
